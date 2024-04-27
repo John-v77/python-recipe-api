c1bc81335f3378037c3f4532c5faf62053343395
JohnV1a <ionut_vasile85@hotmail.com>
2024-04-27 15:41:19 -0400

    Creates recipe app

---
+++ app/recipe/views.py
@@ -0,0 +1,101 @@
+"""
+Views for the recipe APIs
+"""
+
+from rest_framework import (
+    viewsets,
+    #Tag
+    mixins,
+)
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from core.models import (
+    Recipe,
+    #Tag
+    Tag,
+    # Ingredient
+    Ingredient,
+    )
+from recipe import serializers
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """View for manage recipe APIs."""
+    serializer_class = serializers.RecipeDetailSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Retrieve recipes for authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def get_serializer_class(self):
+        """Return the serializer class for request."""
+        if self.action == 'list':
+            return serializers.RecipeSerializer
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create a new recipe."""
+        serializer.save(user=self.request.user)
+
+
+class BaseRecipeAttrViewSet(
+                mixins.DestroyModelMixin,
+                mixins.UpdateModelMixin,
+                mixins.ListModelMixin,
+                viewsets.GenericViewSet):
+    """Base viewset for recipe attributes."""
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter queryset to authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+# #Tag - any misspells bellow will result in issue very hard to debug
+# class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+#     """Manage tags in the database."""
+#     serializer_class = serializers.TagSerializer
+#     queryset = Tag.objects.all()
+#     authentication_classes = [TokenAuthentication]
+#     permission_classes = [IsAuthenticated]
+
+#     def get_queryset(self):
+#         """Filter queryset to authenticated user."""
+
+#         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+# #updating tag commit 1231 - doulbe function
+# class TagViewSet(
+#                 mixins.UpdateModelMixin,
+#                 mixins.ListModelMixin,
+#                 viewsets.GenericViewSet):
+#     """Manage tags in the database."""
+#     serializer_class = serializers.TagSerializer
+#     queryset = Tag.objects.all()
+#     authentication_classes = [TokenAuthentication]
+#     permission_classes = [IsAuthenticated]
+
+#     def get_queryset(self):
+#         """Filter queryset to authenticated user."""
+
+#         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+
+
+
+
+#delete tag commit 1232 - doubled function
+class TagViewSet(BaseRecipeAttrViewSet):
+    """Manage tags in the database."""
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+
+
+class IngredientViewSet(BaseRecipeAttrViewSet):
+    """Manage ingredients in the database."""
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
