18e9ffb0ff336eb58d335b161b3a9c20b91aa34c
JohnV1a <ionut_vasile85@hotmail.com>
2024-04-28 20:53:01 -0400

    adds tag model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -9,6 +9,10 @@ from django.contrib.auth import get_user_model
 
 from core import models
 
+def create_user(email='user@example.com', password='testpas123'):
+    """Create and return an new user."""
+    return get_user_model().objects.create_user(email, password)
+
 class ModelTest(TestCase):
     """Test models."""
 
@@ -69,3 +73,10 @@ class ModelTest(TestCase):
          )
 
          self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creating a tag is successful."""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
